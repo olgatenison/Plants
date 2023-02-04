f79d6f72d08c45ddd364bfeb007427721be41f80
olgatenison <88651744+olgatenison@users.noreply.github.com>
2023-02-04 22:19:39 +0200

    update select js

--- js/select.js
+++ js/select.js
@@ -1,10 +1,31 @@
 //Change list button inner text
-const listItems = document.getElementsByClassName("option__list");
-var x = document.getElementsByClassName("option__list").checked;
-console.log(x);
+/*const listItems = document.getElementsByClassName("option__list");
 
 for (let i = 0; i < listItems.length; ++i) {
   listItems[i].addEventListener("click", function () {
     document.getElementsByClassName("contacts__text")[0].innerHTML = this.title;
+  });
+}
+*/
+
+/*
+for (let i = 0; i < radioButtons.length; ++i) {
+  radioButtons[i].addEventListener("change", function () {
+    if (this.checked) {
+      document.getElementsByClassName("contacts__text")[0].textContent =
+        this.parentElement.textContent;
+    }
+  });
+}*/
+
+const radioButtons = document.getElementsByClassName("option__list");
+const target = document.getElementsByClassName("contacts__text")[0];
+console.log(target);
+for (let i = 0; i < radioButtons.length; ++i) {
+  radioButtons[i].addEventListener("change", function () {
+    console.log("Event triggered");
+    if (this.checked) {
+      target.textContent = this.parentElement.textContent;
+    }
   });
 }
