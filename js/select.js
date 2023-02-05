0cfb194d0ab3935a593aec86631cafa7b6d4e71f
olgatenison <88651744+olgatenison@users.noreply.github.com>
2023-02-06 00:01:05 +0200

    add 2 part js select

--- js/select.js
+++ js/select.js
@@ -1,6 +1,45 @@
 const cityBtn = document.querySelector(".contacts__text");
 const itemCity = document.querySelectorAll(".option__list");
 const listContainer = document.querySelector(".option__container");
+const cityAddresses = document.querySelectorAll(".option__address");
+
+//make all dives hiden
+cityAddresses.forEach((address) => {
+  address.style.display = "none";
+});
+
+//add isOpen class for choosing container - make visible container
+
+cityBtn.addEventListener("click", () => {
+  listContainer.classList.toggle("isOpen");
+});
+
+//change the city name on btn
+itemCity.forEach((item) => {
+  item.addEventListener("click", () => {
+    cityBtn.textContent = item.title;
+    listContainer.classList.remove("isOpen");
+
+    //get choosing (data-city-id="n")
+    const cityId = item.getAttribute("data-city-id");
+    const selectedCity = `option__address address address-${cityId}`;
+
+    //change the visible div (data-city-id="n")=(class address-n)
+    cityAddresses.forEach((address) => {
+      if (address.classList.contains(selectedCity.split(" ")[2])) {
+        address.style.display = "block";
+      } else {
+        address.style.display = "none";
+      }
+    });
+  });
+});
+
+//just btn select
+/*
+const cityBtn = document.querySelector(".contacts__text");
+const itemCity = document.querySelectorAll(".option__list");
+const listContainer = document.querySelector(".option__container");
 
 cityBtn.addEventListener("click", () => {
   cityBtn.classList.toggle("isOpen");
@@ -14,3 +53,4 @@ cityBtn.addEventListener("click", () => {
     });
   });
 });
+*/
