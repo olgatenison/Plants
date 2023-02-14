e94a0c28896ced96e8461a64a6b35f5c68f1afea
olgatenison <88651744+olgatenison@users.noreply.github.com>
2023-02-14 17:14:23 +0200

    add  blur on btn servise

--- js/service.js
+++ js/service.js
@@ -1,67 +1,18 @@
 const serviseBtn = document.querySelectorAll(".service__button");
-console.log(serviseBtn);
 const cards = document.querySelectorAll(".service__item");
-console.log(cards);
-
-let activeButtons = 0;
+console.log(cards.classList);
 
 serviseBtn.forEach((button) => {
-  button.addEventListener("click", (e) => {
-    if (activeButtons < 2) {
-      const buttonText = button.textContent;
-      cards.forEach((card) => {
-        if (card.dataset.title === buttonText) {
-          card.classList.add("active");
-          activeButtons++;
-        }
-      });
-    }
-  });
-});
-
-/* 
-serviseBtn.forEach((button) => {
-  // loop through each button and add a click event listener
   button.addEventListener("click", (e) => {
     const buttonText = button.textContent;
-    console.log(button.textContent);
 
     cards.forEach((card) => {
+      card.classList.remove("outOfBlur");
       if (card.dataset.title === buttonText) {
-        card.classList.add("active");
+        card.classList.add("outOfBlur");
       } else {
-        card.classList.remove("active");
+        card.classList.add("onBlur");
       }
     });
   });
 });
-
- 
-    // remove active class from all buttons
-    serviseBtn.forEach((b) => b.classList.remove("active"));
-    // add active class to clicked button
-    e.target.classList.add("active");
-    // loop through each card
-    cards.forEach((card) => {
-      // if the card's title matches the text of the clicked button, show the card
-      if (
-        card.querySelector(".card__title").textContent === e.target.textContent
-      ) {
-        card.style.display = "block";
-      } else {
-        card.style.display = "none";
-      }
-    });
-  });
-});
-
-add focus and blur event listeners to the cards
-cards.forEach((card) => {
-  card.addEventListener("focus", (e) => {
-    e.target.style.backgroundColor = "lightgray";
-  });
-  card.addEventListener("blur", (e) => {
-    e.target.style.backgroundColor = "white";
-  });
-});
-*/
