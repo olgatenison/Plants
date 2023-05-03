d47755ef2a85bf72a8b8cef3204e86fd9d3aa2b6
olgatenison <88651744+olgatenison@users.noreply.github.com>
2023-05-03 09:37:03 +0300

    add some animation

--- js/service.js
+++ js/service.js
@@ -17,8 +17,7 @@ serviseBtn.forEach((button) => {
   });
 });*/
 
-
-/*const serviceBtns = document.querySelectorAll(".service__button");
+const serviceBtns = document.querySelectorAll(".service__button");
 const serviceCards = document.querySelectorAll(".service__item");
 
 let activeBtn = null;
@@ -56,49 +55,47 @@ serviceBtns.forEach((btn) => {
       btn.classList.toggle("active");
     }
   });
-});*/
+});
 
+// const serviceBtns = document.querySelectorAll(".service__button");
+// const serviceCards = document.querySelectorAll(".service__item");
 
+// // создаем переменную activeBtn, которая будет использоваться для хранения ссылки на активную кнопку на странице
+// let activeBtn = null;
 
-const serviceBtns = document.querySelectorAll(".service__button");
-const serviceCards = document.querySelectorAll(".service__item");
+// // создаем функцию setActiveBtn, которая будет использоваться для установки активной кнопки на странице
+// function setActiveBtn(btn) {
 
-// создаем переменную activeBtn, которая будет использоваться для хранения ссылки на активную кнопку на странице
-let activeBtn = null;
+//   // если на странице уже есть активная кнопка, то мы должны ее сначала деактивировать
+//   if (activeBtn) {
+//     // если переданная кнопка совпадает с активной кнопкой, то мы должны ее деактивировать
+//     if (activeBtn === btn) {
+//       activeBtn.classList.remove("active"); // удаляем класс "active" с активной кнопки
+//       activeBtn = null; // устанавливаем переменную activeBtn в null
+//     }
+//     // если переданная кнопка не совпадает с активной кнопкой, то мы должны сначала деактивировать активную кнопку, а затем активировать переданную кнопку
+//     else {
+//       setActiveBtn(activeBtn); // рекурсивно вызываем функцию setActiveBtn с текущей активной кнопкой, чтобы ее деактивировать
+//     }
+//   }
 
-// создаем функцию setActiveBtn, которая будет использоваться для установки активной кнопки на странице
-function setActiveBtn(btn) {
+//   // если переданная кнопка не null, то мы должны активировать ее
+//   if (btn) {
+//     btn.classList.add("active"); // добавляем класс "active" на переданную кнопку
+//     activeBtn = btn; // сохраняем ссылку на переданную кнопку в переменную activeBtn
+//   }
 
-  // если на странице уже есть активная кнопка, то мы должны ее сначала деактивировать
-  if (activeBtn) {
-    // если переданная кнопка совпадает с активной кнопкой, то мы должны ее деактивировать
-    if (activeBtn === btn) {
-      activeBtn.classList.remove("active"); // удаляем класс "active" с активной кнопки
-      activeBtn = null; // устанавливаем переменную activeBtn в null
-    } 
-    // если переданная кнопка не совпадает с активной кнопкой, то мы должны сначала деактивировать активную кнопку, а затем активировать переданную кнопку
-    else {
-      setActiveBtn(activeBtn); // рекурсивно вызываем функцию setActiveBtn с текущей активной кнопкой, чтобы ее деактивировать
-    }
-  }
+//   // переключаем классы "outOfBlur" и "onBlur" на элементах, имеющих класс "service__item", в зависимости от того, соответствует ли значение атрибута "data-title" элемента значению текста переданной кнопки.
+//   serviceCards.forEach((card) => {
+//     card.classList.toggle("outOfBlur", card.dataset.title === btn?.textContent);
+//     card.classList.toggle("onBlur", card.dataset.title !== btn?.textContent);
+//   });
+// }
 
-  // если переданная кнопка не null, то мы должны активировать ее
-  if (btn) {
-    btn.classList.add("active"); // добавляем класс "active" на переданную кнопку
-    activeBtn = btn; // сохраняем ссылку на переданную кнопку в переменную activeBtn
-  }
+// // добавляем обработчик событий "click" на все элементы с классом "service__button"
+// serviceBtns.forEach((btn) => {
+//   btn.addEventListener("click", (e) => {
+//     const isMultiple = e.metaKey || e.ctrlKey || e.shiftKey; // проверяем, нажата ли клавиша "meta", "ctrl" или "shift"
+//     const isActive = btn === activeBtn; // проверяем, является ли нажатая кнопка уже активной
 
-  // переключаем классы "outOfBlur" и "onBlur" на элементах, имеющих класс "service__item", в зависимости от того, соответствует ли значение атрибута "data-title" элемента значению текста переданной кнопки.
-  serviceCards.forEach((card) => {
-    card.classList.toggle("outOfBlur", card.dataset.title === btn?.textContent);
-    card.classList.toggle("onBlur", card.dataset.title !== btn?.textContent);
-  });
-}
-
-// добавляем обработчик событий "click" на все элементы с классом "service__button"
-serviceBtns.forEach((btn) => {
-  btn.addEventListener("click", (e) => {
-    const isMultiple = e.metaKey || e.ctrlKey || e.shiftKey; // проверяем, нажата ли клавиша "meta", "ctrl" или "shift"
-    const isActive = btn === activeBtn; // проверяем, является ли нажатая кнопка уже активной
-
-    // если не нажата ни одна из клавиш "meta", "ctrl" или "shift", то мы должны установить переданную кнопку как активную (и деактивировать предыдущую активную кнопку
+//     // если не нажата ни одна из клавиш "meta", "ctrl" или "shift", то мы должны установить переданную кнопку как активную (и деактивировать предыдущую активную кнопку
